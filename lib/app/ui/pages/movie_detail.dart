ff20f43b29a6c554badaa255f5c4ed5f75b85b33
dali <lionelviperdali@gmail.com>
2021-08-10 23:00:59 +0100

    fix  : fix overflow text in detail movie

--- lib/app/ui/pages/movie_detail.dart
+++ lib/app/ui/pages/movie_detail.dart
@@ -38,101 +38,105 @@ class MovieDetailCore extends HookWidget {
     }, [detailVM]);
     return Scaffold(
       primary: true,
-      body: NestedScrollView(
-        headerSliverBuilder: (ctx, innerBoxIsScrolled) {
-          return [
-            SliverAppBar(
-              forceElevated: innerBoxIsScrolled,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () => context.pop(),
+            ),
+            actions: [
+              Padding(
+                padding: EdgeInsets.only(
+                  right: 8,
+                  top: 5,
+                  bottom: 5,
+                ),
+                child: IconButton(
+                  onPressed: () async {
+                    int response = await showDialog(
+                      context: context,
+                      builder: (ctx) {
+                        return ActionDialog(
+                          action: () async {
+                            int response = 0;
+                            if (!detailVM.isFav) {
+                              response = await detailVM.addToFavorite();
+                            } else {
+                              response = await detailVM.removeFromFavorite();
+                            }
 
-              leading: IconButton(
-                icon: Icon(Icons.arrow_back_ios),
-                onPressed: () => context.pop(),
-              ),
-              actions: [
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: 8,
-                    top: 5,
-                    bottom: 5,
-                  ),
-                  child: IconButton(
-                    onPressed: () async {
-                      int response = await showDialog(
-                        context: context,
-                        builder: (ctx) {
-                          return ActionDialog(
-                            action: () async {
-                              int response = 0;
-                              if (!detailVM.isFav) {
-                                response = await detailVM.addToFavorite();
-                              } else {
-                                response = await detailVM.removeFromFavorite();
-                              }
-
-                              Navigator.pop(ctx, response);
-                            },
-                          );
-                        },
-                      );
-                      var message = "";
-                      switch (response) {
-                        case 200:
-                          message = detailVM.isFav
-                              ? MyAppLocalizations.of(context)!
-                                  .successRemoveFromFav
-                              : MyAppLocalizations.of(context)!.successAddToFav;
-                          break;
-                        case 400:
-                          message = detailVM.isFav
-                              ? MyAppLocalizations.of(context)!
-                                  .failedToRemoveFromFav
-                              : MyAppLocalizations.of(context)!
-                                  .failedToAddToFav;
-
-                          break;
-                      }
-                      ScaffoldMessenger.of(context)
-                          .showSnackBar(SnackBar(content: Text(message)));
-                      detailVM.setIsFav(!detailVM.isFav);
-                    },
-                    iconSize: 24,
-                    icon: Selector<DetailMovieViewModel, bool>(
-                      selector: (ctx, vm) => vm.isFav,
-                      builder: (ctx, isFav, _) {
-                        return Icon(
-                          isFav ? Icons.bookmark : Icons.bookmark_border,
-                          color: isFav ? Colors.amber : null,
-                          size: 24,
+                            Navigator.pop(ctx, response);
+                          },
                         );
                       },
-                    ),
+                    );
+                    var message = "";
+                    switch (response) {
+                      case 200:
+                        message = detailVM.isFav
+                            ? MyAppLocalizations.of(context)!
+                            .successRemoveFromFav
+                            : MyAppLocalizations.of(context)!.successAddToFav;
+                        break;
+                      case 400:
+                        message = detailVM.isFav
+                            ? MyAppLocalizations.of(context)!
+                            .failedToRemoveFromFav
+                            : MyAppLocalizations.of(context)!
+                            .failedToAddToFav;
+
+                        break;
+                    }
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(SnackBar(content: Text(message)));
+                    detailVM.setIsFav(!detailVM.isFav);
+                  },
+                  iconSize: 24,
+                  icon: Selector<DetailMovieViewModel, bool>(
+                    selector: (ctx, vm) => vm.isFav,
+                    builder: (ctx, isFav, _) {
+                      return Icon(
+                        isFav ? Icons.bookmark : Icons.bookmark_border,
+                        color: isFav ? Colors.amber : null,
+                        size: 24,
+                      );
+                    },
                   ),
                 ),
-              ],
-              pinned: true,
-              flexibleSpace: FlexibleAppBarMovieDetail(),
-              expandedHeight: 256,
-              //backgroundColor: Colors.transparent,
-            ),
-          ];
-        },
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  MoreInformationMovie(),
-                ],
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0),
-                child: OverviewDetailMovie(),
-              )
             ],
+            pinned: true,
+            flexibleSpace: FlexibleAppBarMovieDetail(),
+            expandedHeight: 256,
+            //backgroundColor: Colors.transparent,
           ),
-        ),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            sliver: SliverFillRemaining(
+              fillOverscroll: true,
+              hasScrollBody: true,
+              child: SingleChildScrollView(
+                physics: NeverScrollableScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Row(
+                      children: [
+                        MoreInformationMovie(),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: OverviewDetailMovie(),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
