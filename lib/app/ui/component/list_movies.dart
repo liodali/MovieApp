68ddb7abaa7d46a13b46cc9801afab5d228dcf45
dali <lionelviperdali@gmail.com>
2021-08-17 14:11:35 +0100

    replace statfulwidget by hookwidget

--- lib/app/ui/component/list_movies.dart
+++ lib/app/ui/component/list_movies.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:movie_app/domain/models/movie.dart';
 import 'package:provider/provider.dart';
 
@@ -8,36 +9,19 @@ import '../widget/loading_widget.dart';
 import '../widget/stream_component.dart';
 import 'item_movie.dart';
 
-class ListMovies extends StatefulWidget {
+class ListMovies extends HookWidget {
   ListMovies({
     Key? key,
   }) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() => _StateListMovies();
-}
-
-class _StateListMovies extends State<ListMovies> {
-  MoviesViewModel? viewModel;
-
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration.zero, () async {
-      viewModel!.initMovies();
-    });
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    if (viewModel == null) viewModel = Provider.of<MoviesViewModel>(context);
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final viewModel = Provider.of<MoviesViewModel>(context);
+    useEffect(() {
+      viewModel.initMovies();
+    }, [context]);
     return StreamComponent<List<Movie>>(
-      stream: viewModel!.stream,
+      stream: viewModel.stream,
       loading: SliverFillRemaining(
         fillOverscroll: false,
         hasScrollBody: false,
